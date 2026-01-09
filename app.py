cafa64f7c44e9d81f297f89fe6ee83a8f891cbda
domtiago <tgotrading@gmail.com>
2026-01-08 22:33:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-from utils.extract_contact import extract_contact
-from utils.contact_repository import upsert_contact, insert_contact_notes
-
-
 import os
 from datetime import datetime, time
+
 import streamlit as st
 from supabase import create_client
 
-# -----------------------------
-# Config
-# -----------------------------
-st.set_page_config(page_title="ZiaFlow", page_icon="✨", layout="wide")
+from utils.extract_contact import extract_contact
+from utils.contact_repository import upsert_contact, insert_contact_notes
 
-SUPABASE_URL = st.secrets.get("SUPABASE_URL") or os.getenv("SUPABASE_URL")
-SUPABASE_KEY = st.secrets.get("SUPABASE_KEY") or os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
 
 
 #--------------------------------------------
